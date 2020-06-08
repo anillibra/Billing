3c696463e0a2ef139f38fb216f40820e09fd7660
anillibra <anillibra76@gmail.com>
2020-06-08 22:44:44 +0530

    Initial Commit

---
+++ Billing/Controllers/BillingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Billing.Controllers
+{
+    [EnableCors("CORS")]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BillingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Billing-No-101", "Billing-No-102" };
+        }
+    }
+}
