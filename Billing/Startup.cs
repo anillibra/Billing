4a0393ccfde738d15807074f6f2b68d63275955b
anillibra <anillibra76@gmail.com>
2020-06-08 22:47:09 +0530

    update status

--- Billing/Startup.cs
+++ Billing/Startup.cs
@@ -17,6 +17,7 @@ namespace Billing
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
